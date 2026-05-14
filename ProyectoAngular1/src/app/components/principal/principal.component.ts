af8c33ec545e66f95485a0ed86e86d1c2a5d71b8
Onlinebusiness <onlinebusinessfy@gmail.com>
2026-05-14 10:57:59 -0700

    Mayo 14

--- ProyectoAngular1/src/app/components/principal/principal.component.ts
+++ ProyectoAngular1/src/app/components/principal/principal.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-principal',
-    templateUrl: './principal.component.html',
-    styleUrls: ['./principal.component.css'],
-    standalone: false
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css'],
+  standalone: false
 })
 export class PrincipalComponent implements OnInit {
 
@@ -13,4 +13,23 @@ export class PrincipalComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  miTarjeta1 = {
+    "titulo": "Tarjeta de Producto 1",
+    "contenido": "Este es el contenido de prueba 1",
+    "boton": "Aceptar Obj1"
+  };
+
+  miTarjeta2 = {
+    "titulo": "Tarjeta de Producto 2",
+    "contenido": "Este es el contenido de prueba 2",
+    "boton": "Aceptar Obj2"
+  };
+
+  onMyVarOutput(event: number) {
+    console.log("Valor recibido:", event);
+  }
+
+  eventoRecibido(event: number) {
+    console.log("El valor recibido es: " + event);
+  }
 }
--- ProyectoAngular1/src/app/components/tarjeta/tarjeta.component.ts
+++ ProyectoAngular1/src/app/components/tarjeta/tarjeta.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector: 'app-tarjeta',
-    templateUrl: './tarjeta.component.html',
-    styleUrls: ['./tarjeta.component.css'],
-    standalone: false
+  selector: 'app-tarjeta',
+  templateUrl: './tarjeta.component.html',
+  styleUrls: ['./tarjeta.component.css'],
+  standalone: false
 })
 export class TarjetaComponent implements OnInit {
 
@@ -15,9 +15,21 @@ export class TarjetaComponent implements OnInit {
 
   @Input()
   titulo = "Tarjeta de Producto";
+
   @Input()
   contenido = "Este es el contenido de prueba";
+
   @Input()
   boton = "Este btn prueba";
-}
 
+  @Output()
+  myVarOutput = new EventEmitter<number>();
+
+  contador: number = 0;
+
+  seleccionarTarjeta() {
+    this.contador += 1;
+    this.myVarOutput.emit(this.contador);
+    console.log("Presionaste sobre la tarjeta");
+  }
+}
